bb6981a45c338891df337341ed16241be2b4882a
neilZon <neilcviloria@gmail.com>
2020-05-09 22:08:04 -0600

    connected to mongoDB

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,16 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
+//landing page
+router.get('/', (req, res) => {
+    res.send("voucher landing page");
+});
+
 // register page
 router.get('/register', (req, res) => {
-    res.send("this is the register page")
+    res.send("this is the register page");
 });
 
 // login page
 router.get('/login', (req, res) => {
-    res.send("this is the login page")
-})
+    res.send("this is the login page");
+});
 
 // login authentication
+router.post('/login', (req, res) => {
+    res.send("login authentication");
+});
 
-module.exports = router
+module.exports = router; 
--- api/server.js
+++ api/server.js
@@ -1,14 +1,23 @@
-//==========  server.js  ===========
-const express = require('express') // express app
+//=====================  server.js  ======================
+
+const express = require('express'); // express app
+const mongoose = require('mongoose');
 
 const app = express();
 
-// routing 
+//MIDDLEWARE
+app.use('/login', (req, res, next) => {
+    console.log("howdy");
+    next();
+})
+
+//ROUTES
 app.use('/', require('./routes/index'));
 
-app.get('/', (req, res) => {
-    res.send("welcome to voucher");
-});
+//CONNECT TO DB
+mongoose.connect('mongodb+srv://voucher-user0:<password>@vouchercluster0-k7dyp.mongodb.net/test?retryWrites=true&w=majority', () => {
+    console.log('Connection success');
+})
 
 const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => console.log(`server started on port ${PORT} `));
+app.listen(PORT, () => console.log(`server started on port ${PORT}`));
